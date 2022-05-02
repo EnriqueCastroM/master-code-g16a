792dc115eb1db713618ac62c644fb82444355128
IvanCastro <kcmtha@gmail.com>
2022-05-02 12:28:20 -0500

    28 abril 2022

---
+++ 02.IntroAJs/Ejercicio1/app.js
@@ -0,0 +1,14 @@
+var numEntrada;
+var i=0;
+var parEncontrado=0;
+numEntrada = prompt('Ingresa tu numero:');
+
+while(parEncontrado<=100)
+{
+if(par%2 === 0){
+    
+} 
+par++
+}
+    
+alert(`los numeros pares del 0 a ${numEntrada}, son: ${parEncontrado}`);
