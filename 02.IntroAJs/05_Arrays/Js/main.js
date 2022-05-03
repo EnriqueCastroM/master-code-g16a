e933b6039731dafd1e52e7614bccac16b5819c0f
IvanCastro <kcmtha@gmail.com>
2022-05-02 21:31:36 -0500

    02 mayo 2022

---
+++ 02.IntroAJs/05_Arrays/Js/main.js
@@ -0,0 +1,96 @@
+    // EJEMPLO INDICES ARRAYS
+
+// var arrays = [];//esta vacio
+// var valores = ['hola', 'mascota', 'comida', 'adios'];
+
+// for(var i=0; i < valores.length; i++){
+//     console.log(valores[i])
+// }
+// ---
+
+
+    // EJEMPLO MASCOTAS ARRAYS
+
+// var mascota, mascota1, mascota2, cadenaMascota;
+// var arrayMascotas = [];
+
+// mascota = prompt('Ingresa el nombre de tu mascota 1:');
+// mascota1 = prompt('Ingresa el nombre de tu mascota 2:');
+// mascota2 = prompt('Ingresa el nombre de tu mascota 3:');
+
+// if(mascota, mascota1, mascota2 === ''){
+
+//     alert('Porfavor ingresa el de nuevo el campo correctamente');
+
+// }
+// else{
+
+//     arrayMascotas.push(mascota, mascota1, mascota2);
+
+//     cadenaMascota = `El nombre de las mascotas: ${arrayMascotas[0]}, ${arrayMascotas[1]}, ${arrayMascotas[2]}`;
+//     alert(cadenaMascota);
+// }
+
+//-----------EJERCICIO------------------
+//1-mediante ciclo for mostrar todos los platillos
+//2-mediante un for o if mostar solo el valor 3 y 5
+//3-mediante un for o if mostar los elementos impares
+//4-mediante un for o if mostrar los elementos pares
+
+var platillos = ['birria', 'tacos', 'hamburguesas', 'lonche', 'hotdogs', 'pasta fetuchini'];
+
+// /*Ejecicio 1*/
+// for(var i=0; i<platillos.length; i++){
+//     document.write(platillos[i]);
+// }
+
+
+/*Ejercicio 2*/
+// for(var i=0; i<platillos.length; i++){
+//     if(platillos[i] === platillos[3] || platillos[i] === platillos[5]){
+//         console.log(platillos[i]);
+//     }
+// }
+
+
+// /*Ejercicio 3*/
+//  for(var i=0; i<platillos.length; i++)
+//  {
+
+// con IF y redisuales
+//     if(i%2 != 0){
+//         console.log(platillos[i]);
+//     }
+    
+// }
+//segunda forma de hacerlo con incrementar impares desde el index
+// for(var i=0; i<platillos.length; i+=2){
+//     console.log(platillos[i]);
+// }
+
+/*Ejercicio 4*/
+// for(var i=0; i<platillos.length; i++){
+//     if(i%2 === 0){
+//         console.log(platillos[i]);
+//     }
+// }
+
+
+
+
+
+
+
+
+   
+    
+
+
+
+
+
+
+
+
+
+
