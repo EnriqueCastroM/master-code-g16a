853484efa8fdfba67f58d375f5e6920765a60a0f
IvanCastro <kcmtha@gmail.com>
2022-05-05 16:46:30 -0500

    Proyecto

--- 02.IntroAJs/07_Scope/js/main.js
+++ 02.IntroAJs/07_Scope/js/main.js
@@ -1,11 +1,31 @@
-var color = 'negro';
+// var color = 'negro';
 
-function pinturas(){
-var color = 'rojo';
-console.log(color);
-var listaDeColores = 'Mi color favorito es:' + color;
-console.log(listaDeColores)
-}
+// function pinturas(){
+//     var color = 'rojo';
+//     console.log(color);
 
-pinturas(color);
-console.log(color);
+//     var listaDeColores = 'Mi colore favorito es: ' + color;
+//     console.log(listaDeColores);
+// }
+
+// console.log(color);
+
+// pinturas(color);
+
+// var edadActual = 4;
+
+// function calcularDelBebe(){
+//     var edadTotal = ('una operacion');
+//     var suma = edadTotal + edadActual;
+
+//     function dias(){
+//         var diasDeNacido = 5;
+//         edadActual = diasDeNacido - 8;
+        
+
+//     }
+
+//     console.log(edadTotal);
+// }
+// calcularDelBebe();
+// console.log(edadTotal);
